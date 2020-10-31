07cd59c9d61eabf73fdc9c19b52f616245e1c289
Xi-You <71115574+Xi-You@users.noreply.github.com>
2020-10-31 16:30:05 +0800

    fix

--- android/src/com/gdx/cellular/AndroidLauncher.java
+++ android/src/com/gdx/cellular/AndroidLauncher.java
@@ -4,13 +4,12 @@ import android.os.Bundle;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
-import com.gdx.cellular.CellularAutomaton;
 
 public class AndroidLauncher extends AndroidApplication {
 	@Override
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
-		initialize(new CellularAutomaton(), config);
+		initialize(new com.gdx.cellular.Launcher(), config);
 	}
 }
